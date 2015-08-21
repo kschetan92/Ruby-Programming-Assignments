5f414a0b4c8090d8d85b08cbd4964349ebed4c6f
chetan K S <chetan.ks@v2solutions.com>
2015-08-21 18:38:13 +0530

    removing the inspect method

--- EmployeeSort/Employee.rb
+++ EmployeeSort/Employee.rb
@@ -1,31 +1,18 @@
-require "test/unit"
-
 class Employee
-include Comparable
-attr_accessor :name
+  
+  include Comparable
+  attr_accessor :name
 
-  def <=>nextEmployeeObject
-    self.name <=> nextEmployeeObject.name
-  end
+    def <=>(other_Employee)
+      self.name <=> other_Employee.name
+    end
 
-  def inspect
-    "#{@name}, #{@age}, #{@address}"
-  end
+    def initialize(name, age, address)
+      @name = name
+      @age = age
+      @address = address
+    end
 
-  def initialize(name, age, address)
-    @name = name
-    @age = age
-    @address = address
-  end
-
-end
-
-class TestEmployee < Test::Unit::TestCase
-
-  def test_EmployeeSort
-    employee = [Employee.new("Chetan", 23,"Bangalore"), Employee.new("Akash", 27,"Mangalore")]
-    assert_equal([Akash, 27, Mangalore, Chetan, 23, Bangalore], employee.sort)
-  end
 end
 
 emp = [Employee.new("Chetan", 23,"Bangalore"), Employee.new("Akash", 27,"Mangalore")]
