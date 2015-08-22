353da9b7a097b0e87c6fd38d53cd1ef0f64a9bd2
chetan K S <chetan.ks@v2solutions.com>
2015-08-22 14:40:29 +0530

    Updating Employee.rb with test cases

--- EmployeeSort/Employee.rb
+++ EmployeeSort/Employee.rb
@@ -1,19 +1,32 @@
+require "test/unit"
+
 class Employee
-  
-  include Comparable
-  attr_accessor :name
+include Comparable
+attr_accessor :name
 
-    def <=>(other_Employee)
-      self.name <=> other_Employee.name
-    end
+  def <=>(other_Employee)
+    self.name <=> other_Employee.name
+  end
 
-    def initialize(name, age, address)
-      @name = name
-      @age = age
-      @address = address
-    end
+  def initialize(name, age, address)
+    @name = name
+    @age = age
+    @address = address
+  end
 
 end
 
-emp = [Employee.new("Chetan", 23,"Bangalore"), Employee.new("Akash", 27,"Mangalore")]
-p emp.sort
+class TestEmployee < Test::Unit::TestCase
+
+def test_employee_test
+
+  emp1 = Employee.new("Chetan", 23,"Bangalore")
+  emp2 = Employee.new("Akash", 27,"Mangalore")
+  emps = [emp1, emp2]
+  emps.sort!
+  assert_equal([emp2, emp1], emps)
+
+end
+end
+# emp1 = [Employee.new("Chetan", 23,"Bangalore").instance_variables, Employee.new("Akash", 27,"Mangalore")]
+# p emps
