616bc25bec38b1ce6247988d8f90685a75c5bae7
chetan K S <chetan.ks@v2solutions.com>
2015-08-25 18:01:04 +0530

    Modifying the program to read different files and accessing method dynamically

--- CSVReader/CSVReaders.rb
+++ CSVReader/CSVReaders.rb
@@ -1,58 +1,48 @@
 class CSVReader
   require "csv"
-  attr_accessor :name, :age, :department
 
   def self.inherited(subclass)
-    @@subclass = subclass
-    @@csv_container = CSV.read("/home/chetan/Desktop/Samples/#{@@subclass}.csv", :headers => true)
-  end
+    @subclass = subclass
+    @csv_container = CSV.read("/home/chetan/Desktop/Samples/#{@subclass}.csv", :headers => true).to_a
+    @headers = CSV.open("/home/chetan/Desktop/Samples/#{@subclass}.csv", 'r') { |csv| csv.first }
 
-  def self.find_by_name(name)
-    @@csv_container.each do |row|
-      if(row[0] == name)
-        process_results(row)
-      end
+    @headers.each do |method| 
+
+      class_eval("attr_accessor :#{method}") 
+
+      class_eval %Q"
+        def self.find_by_#{method}(argument)
+          #{@csv_container}.each do |row|
+            0.upto((row.length)-1) do |index|  
+              if(row[index] == argument)
+                process_results(row)
+              end
+            end
+          end
+        end
+      "
+
+      class_eval %Q"
+        def self.process_results(row)
+          subclass_object = #{@subclass}.new 
+          p subclass_object
+          p subclass_object = row
+        end
+      "
+
     end
-  end
-
-  def self.find_by_age(age)
-    @@csv_container.each do |row|
-      if(row[1] == age)
-        process_results(row)
-      end
-    end
-  end
-
-  def self.find_by_department(department)
-    @@csv_container.each do |row|
-      if(row[2] == department)
-        process_results(row)
-      end
-    end
-  end
-
-  def self.process_results(row)
-    subclass_object = @@subclass.new
-    subclass_object.name = row[0]
-    subclass_object.age = row[1].to_i
-    subclass_object.department = row[2]
-    p subclass_object
   end
 
 end
+
+class Students < CSVReader
+
+end
+puts
+Students.find_by_name("Mechanical")
 
 class Employee < CSVReader
 
-
 end
-
-Employee.find_by_name("Chetan")
-Employee.find_by_age("23")
-Employee.find_by_department("ROR")
-
-#Creating a new Employee
-new_employee = Employee.new
-new_employee.name = "Rahul"
-new_employee.age = 34
-new_employee.department = "IT"
-p new_employee
+puts
+Employee.find_by_department("Chetan")
