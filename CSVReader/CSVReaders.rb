44c6c91c439c36c01cf681b5980ce6151bacebad
chetan K S <chetan.ks@v2solutions.com>
2015-08-26 13:27:31 +0530

    Modifying the program to print matching output and also print specific data from the match

--- CSVReader/CSVReaders.rb
+++ CSVReader/CSVReaders.rb
@@ -13,19 +13,22 @@ class CSVReader
           #{@csv_container}.each do |row|
             0.upto((row.length)-1) do |index|  
               if(row[index] == argument)
-                process_results(row)
+                return process_results(row)
               end
             end
           end
         end
       "
   end
-  
+
     class_eval %Q"
         def #{@subclass}.process_results(row)
           subclass_object = #{@subclass}.new 
-          p subclass_object
-          p subclass_object = row
+          #{@headers}.each_with_index do |method, index|
+            method = \"@\"<<method
+            subclass_object.instance_variable_set(method, row[index])
+          end
+          return subclass_object
         end
       "
   end  
@@ -38,5 +41,15 @@ end
 class Students < CSVReader
 end
 
-Students.find_by_name("Rahul")
-Employee.find_by_department("Finance")
+student = Students.find_by_name("Rahul")
+p student
+p student.name
+p student.age
+p student.address
+
+
+employee = Employee.find_by_department("Finance")
+p employee
+p employee.name
+p employee.age
+p employee.department
