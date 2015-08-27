70bc5b1df06bd6af93cdd4810cdddfad28462cdf
chetan K S <chetan.ks@v2solutions.com>
2015-08-27 15:03:48 +0530

    Enhancing the program with adding Exceptions

--- CSVReader/CSVReaders.rb
+++ CSVReader/CSVReaders.rb
@@ -2,37 +2,46 @@ class CSVReader
   require "csv"
 
   def self.inherited(subclass)
-    @subclass = subclass
-    @csv_container = CSV.read("#{@subclass}.csv", :headers => true).to_a
-    @headers = CSV.open("#{@subclass}.csv", 'r') { |csv| csv.first }
-    @headers.each do |method| 
-      class_eval("attr_accessor :#{method}") 
 
-      class_eval %Q"
-        def #{@subclass}.find_by_#{method}(argument)
-          #{@csv_container}.each do |row|
-            0.upto((row.length)-1) do |index|  
-              if(row[index] == argument)
-                return process_results(row)
+    begin
+      @subclass = subclass
+      @csv_container = CSV.read("#{@subclass}.csv", :headers => true).to_a
+      @headers = CSV.open("#{@subclass}.csv", 'r') { |csv| csv.first }
+      @headers.each do |method| 
+        class_eval("attr_accessor :#{method}") 
+
+        class_eval %Q"
+          def #{@subclass}.find_by_#{method}(argument)
+            matched_objects = []
+            #{@csv_container}.each do |row|
+              row.each do |data|  
+                if(data == argument)
+                  matched_objects << process_results(row)
+                end  
               end
             end
+            return matched_objects
           end
-        end
-      "
-  end
+        "
+      end
 
-    class_eval %Q"
-        def #{@subclass}.process_results(row)
-          subclass_object = #{@subclass}.new 
-          #{@headers}.each_with_index do |method, index|
-            method = \"@\"<<method
-            subclass_object.instance_variable_set(method, row[index])
-          end
-          return subclass_object
-        end
-      "
+        class_eval %Q"
+            def #{@subclass}.process_results(row)
+              subclass_object = #{@subclass}.new 
+              #{@headers}.each_with_index do |method, index|
+                method = \"@\"<<method
+                subclass_object.instance_variable_set(method, row[index])
+              end
+              return subclass_object
+            end
+          "
+
+    rescue IOError => e
+      puts "The file #{@subclass}.rb is not available in the specified path, please verify your path and try again."
+    rescue StandardError => s
+      raise
+    end
   end  
-
 end
 
 class Employee < CSVReader
@@ -42,14 +51,7 @@ class Students < CSVReader
 end
 
 student = Students.find_by_name("Rahul")
-p student
-p student.name
-p student.age
-p student.address
-
+student.each { |object| p object }
 
 employee = Employee.find_by_department("Finance")
-p employee
-p employee.name
-p employee.age
-p employee.department
+employee.each { |object| p object }
